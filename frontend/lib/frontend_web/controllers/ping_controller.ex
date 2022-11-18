180457f468c64e4d291a8ca0b98dbdf03607a341
Kriztoper Urmeneta <kriztoper.urmeneta@lawadvisor.com>
2022-11-18 14:51:56 +0800

    change url to host.docker.internal from localhost

--- frontend/lib/frontend_web/controllers/ping_controller.ex
+++ frontend/lib/frontend_web/controllers/ping_controller.ex
@@ -9,7 +9,7 @@ defmodule FrontendWeb.PingController do
     # text(conn, HTTPoison.get "http://0.0.0.0:4001/api/ping")
     # HTTPoison.get "http://0.0.0.0:4001/api/ping"
     # {:ok, response} = HTTPoison.get "http://0.0.0.0:4000/hello"
-    {:ok, response} = HTTPoison.get "http://0.0.0.0:4001/api/ping"
+    {:ok, response} = HTTPoison.get "http://host.docker.internal:4001/api/ping"
     text(conn, response.body)
   end
 
