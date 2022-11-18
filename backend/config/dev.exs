4e97da3c215cfff8f2cdb1f7db1a149bef9557f6
Kriztoper Urmeneta <kriztoper.urmeneta@lawadvisor.com>
2022-11-18 11:50:25 +0800

    set correct port for backend to 4001

--- backend/config/dev.exs
+++ backend/config/dev.exs
@@ -16,7 +16,7 @@ config :backend, Backend.Repo,
 # watchers to your application. For example, we use it
 # with webpack to recompile .js and .css sources.
 config :backend, BackendWeb.Endpoint,
-  http: [port: 4000],
+  http: [port: 4001],
   debug_errors: true,
   code_reloader: true,
   check_origin: false,
