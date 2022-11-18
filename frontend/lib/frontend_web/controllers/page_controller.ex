d724f4faec7bbf5287d33579a2a4bc4928e1bb58
Kriztoper Urmeneta <kriztoper.urmeneta@lawadvisor.com>
2022-11-18 15:34:24 +0800

    show task list on index action

--- frontend/lib/frontend_web/controllers/page_controller.ex
+++ frontend/lib/frontend_web/controllers/page_controller.ex
@@ -2,6 +2,10 @@ defmodule FrontendWeb.PageController do
   use FrontendWeb, :controller
 
   def index(conn, _params) do
-    render(conn, "index.html")
+    {:ok, response} = HTTPoison.get "http://host.docker.internal:4001/api/tasks"
+    {:ok, body} = response.body
+                          |> Jason.decode()
+    # tasks = for {key, val} <- body["data"], into: %{}, do: {String.to_atom(key), val}
+    render(conn, "index.html", tasks: body["data"])
   end
 end
