0583f668f59953d3ea5de69a91887847d88af66a
Kriztoper Urmeneta <kriztoper.urmeneta@lawadvisor.com>
2022-11-28 13:33:34 +0800

    initial client-side implem of sortable drag-and-drop of Tasks

--- frontend/assets/js/app.js
+++ frontend/assets/js/app.js
@@ -135,3 +135,44 @@ tbody.forEach(function(elem) {
 //     // return the TR tag so that the above tbody.append gets an element to actually append
 //     return tr;
 // }));
+
+
+const draggables = document.querySelectorAll('.draggable')
+const containers = document.querySelectorAll('.sortable.container')
+
+draggables.forEach(draggable => {
+    draggable.addEventListener('dragstart', () => {
+        draggable.classList.add('dragging')
+    })
+
+    draggable.addEventListener('dragend', () => {
+        draggable.classList.remove('dragging')
+    })
+})
+
+containers.forEach(container => {
+    container.addEventListener('dragover', e => {
+        e.preventDefault()
+        const afterElement = getDragAfterElement(container, e.clientY)
+        const draggable = document.querySelector('.dragging')
+        if (afterElement == null) {
+            container.appendChild(draggable)
+        } else {
+            container.insertBefore(draggable, afterElement)
+        }
+    })
+})
+
+function getDragAfterElement(container, y) {
+    const draggableElements = [...container.querySelectorAll('.draggable:not(.dragging)')]
+
+    return draggableElements.reduce((closest, child) => {
+        const box = child.getBoundingClientRect()
+        const offset = y - box.top - box.height / 2
+        if (offset < 0 && offset > closest.offset) {
+            return { offset: offset, element: child }
+        } else {
+            return closest
+        }
+    }, { offset: Number.NEGATIVE_INFINITY }).element
+}
