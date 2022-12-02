2d01dfc46822e89848da48ff264e01ef2eb07b8a
Kriztoper Urmeneta <kriztoper.urmeneta@lawadvisor.com>
2022-12-02 16:46:59 +0800

    implement Move List to right-most side of Lists

--- frontend/lib/frontend_web/live/task_live.ex
+++ frontend/lib/frontend_web/live/task_live.ex
@@ -111,6 +111,25 @@ defmodule FrontendWeb.TaskLive do
   def handle_event("reorder_list", %{
     "list_id" => list_id,
     "before_task_position" => before_task_position,
+    "current_task_position" => current_task_position
+  }, socket) do
+
+    new_position_param = D.add(before_task_position, D.new(50))
+
+    task_params = %{"position" => D.to_string(new_position_param)}
+    body = Jason.encode! %{"list" => task_params}
+
+    IO.puts ">------------->>>>> called moving LIST at the middle, before/new position is " <> before_task_position
+            <> ", " <> D.to_string(new_position_param)
+    {:ok, response} = HTTPoison.put "http://host.docker.internal:4001/api/lists/" <> list_id,
+                                    body, [{"Content-Type", "application/json"}]
+
+    {:reply, %{"new_position" => D.to_string(new_position_param)}, socket}
+  end
+
+  def handle_event("reorder_list", %{
+    "list_id" => list_id,
+    "before_task_position" => before_task_position,
     "current_task_position" => current_task_position,
     "after_task_position" => after_task_position
   }, socket) do
