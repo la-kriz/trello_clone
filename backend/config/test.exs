7c65f132da8b33ccdad4b9575251fe0a4babaa31
Kriztoper Urmeneta <kriztoper.urmeneta@lawadvisor.com>
2022-12-05 11:52:55 +0800

    set DB URL for testing

--- backend/config/test.exs
+++ backend/config/test.exs
@@ -6,7 +6,7 @@ use Mix.Config
 # to provide built-in test partitioning in CI environment.
 # Run `mix help test` for more information.
 config :backend, Backend.Repo,
-  url: System.get_env("DATABASE_URL") |> String.replace("?", "test"),
+  url: "postgres://postgres:123456@127.0.0.1:5432/law_advisor_dev", #System.get_env("DATABASE_URL") |> String.replace("?", "test"),
   pool: Ecto.Adapters.SQL.Sandbox
 
 # We don't run a server during test. If one is required,
