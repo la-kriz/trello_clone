1f5057c1a126232565df81fdd9dfaf27ec5b96eb
Kriztoper Urmeneta <kriztoper.urmeneta@lawadvisor.com>
2022-12-07 14:38:26 +0800

    add new changeset fn to User schema for user registration

--- backend/lib/backend/accounts/user.ex
+++ backend/lib/backend/accounts/user.ex
@@ -17,4 +17,19 @@ defmodule Backend.Accounts.User do
     |> validate_required([:username, :email, :password])
     |> unique_constraint(:email)
   end
+
+  def registration_changeset(user, attrs) do
+    user
+    |> cast(attrs, [:username, :email, :password])
+    |> validate_required([:username, :email, :password])
+    |> unique_constraint(:email)
+    |> encrypt_and_put_password()
+  end
+
+  defp encrypt_and_put_password(user) do
+    with password <- fetch_field!(user, :password) do
+      encrypted_password = Bcrypt.Base.hash_password(password, Bcrypt.gen_salt(12, true))
+      put_change(user, :password, encrypted_password)
+    end
+  end
 end
