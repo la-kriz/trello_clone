644405b43f9c650faffe2c799a8c6b17de9192a8
Kriztoper Urmeneta <kriztoper.urmeneta@lawadvisor.com>
2022-12-07 14:43:47 +0800

    add action that registers new user

--- backend/lib/backend_web/controllers/user_controller.ex
+++ backend/lib/backend_web/controllers/user_controller.ex
@@ -6,4 +6,11 @@ defmodule BackendWeb.UserController do
 
   action_fallback BackendWeb.FallbackController
 
+  def register(conn, %{"user" => user_params}) do
+    with {:ok, user} <- Accounts.create_user(user_params) do
+      conn
+      |> put_status(:created)
+      |> text("User successfully registered with email: " <> " " <> user.email)
+    end
+  end
 end
