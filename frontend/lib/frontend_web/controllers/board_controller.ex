583b61dc82bb8511131537bca2ac99adbc8bf7f3
Kriztoper Urmeneta <kriztoper.urmeneta@lawadvisor.com>
2022-12-14 18:47:51 +0800

    fetch boards from API then display to board index

--- frontend/lib/frontend_web/controllers/board_controller.ex
+++ frontend/lib/frontend_web/controllers/board_controller.ex
@@ -2,7 +2,18 @@ defmodule FrontendWeb.BoardController do
   use FrontendWeb, :controller
 
   def index(conn, _params) do
-    render(conn, "index.html")
+
+    user_id = Integer.to_string(get_session(conn, :user_id))
+
+    access_token = get_session(conn, :access_token)
+    headers = [{:"Authorization", "Bearer #{access_token}"}, {:"Content-Type", "application/json"}]
+
+    {:ok, response} = HTTPoison.get "http://host.docker.internal:4001/api/boards/users/" <> user_id, headers
+    {:ok, board_body} = response.body |> Jason.decode()
+
+    data = board_body["data"]
+
+    render(conn, "index.html", boards: data)
   end
 
 end
