66750a6a8b53795a6f0246d6d6755bf9118c1efb
Kriztoper Urmeneta <kriztoper.urmeneta@lawadvisor.com>
2022-12-14 18:31:36 +0800

    add new context user for querying Board

---
+++ backend/lib/backend/user.ex
@@ -0,0 +1,23 @@
+defmodule Backend.User do
+  @moduledoc """
+  The User context.
+  """
+
+  import Ecto.Query, warn: false
+  alias Backend.Repo
+
+  alias Backend.User.Board
+  alias Backend.Access.UserPermission
+
+  def get_boards_user_has_access_to(user_id) do
+    base_query = from(boards in Board);
+    boards_in_user_permissions_filtered_by_user_id = from(
+      board in base_query,
+      join: user_permission in UserPermission,
+      on: user_permission.board_id == board.id,
+      where: user_permission.user_id == ^user_id
+    )
+    Repo.all(boards_in_user_permissions_filtered_by_user_id)
+  end
+
+end
