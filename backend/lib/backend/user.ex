503c47bbbfce7c3c4ca157023b8c5f546343a769
Kriztoper Urmeneta <kriztoper.urmeneta@lawadvisor.com>
2022-12-14 18:46:47 +0800

    temporarily exclude filtering in get boards

--- backend/lib/backend/user.ex
+++ backend/lib/backend/user.ex
@@ -11,13 +11,13 @@ defmodule Backend.User do
 
   def get_boards_user_has_access_to(user_id) do
     base_query = from(boards in Board);
-    boards_in_user_permissions_filtered_by_user_id = from(
-      board in base_query,
-      join: user_permission in UserPermission,
-      on: user_permission.board_id == board.id,
-      where: user_permission.user_id == ^user_id
-    )
-    Repo.all(boards_in_user_permissions_filtered_by_user_id)
+#    boards_in_user_permissions_filtered_by_user_id = from(
+#      board in base_query
+#      join: user_permission in UserPermission,
+#      on: user_permission.board_id == board.id,
+#      where: user_permission.user_id == ^user_id
+#    )
+    Repo.all(base_query)
   end
 
 end
