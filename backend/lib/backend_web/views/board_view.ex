8554bc385230d3e0301bc62074b8725df19e3d2f
Kriztoper Urmeneta <kriztoper.urmeneta@lawadvisor.com>
2022-12-14 18:21:54 +0800

    add a board view for rendering board as json

---
+++ backend/lib/backend_web/views/board_view.ex
@@ -0,0 +1,17 @@
+defmodule BackendWeb.BoardView do
+  use BackendWeb, :view
+  alias BackendWeb.BoardView
+
+  def render("index.json", %{boards: boards}) do
+    %{data: render_many(boards, BoardView, "board.json")}
+  end
+
+  def render("show.json", %{board: board}) do
+    %{data: render_one(board, BoardView, "board.json")}
+  end
+
+  def render("board.json", %{board: board}) do
+    %{id: board.id,
+      title: board.title}
+  end
+end
