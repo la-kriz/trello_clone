fc0c93ec3a5ff4b35a89c95f733c6e80168b6b20
Kriztoper Urmeneta <kriztoper.urmeneta@lawadvisor.com>
2022-12-15 16:47:10 +0800

    fetch permission by user and board before showing board

--- frontend/lib/frontend_web/controllers/board_controller.ex
+++ frontend/lib/frontend_web/controllers/board_controller.ex
@@ -20,8 +20,19 @@ defmodule FrontendWeb.BoardController do
 
   def show(conn, %{"board_id" => board_id, "board_title" => board_title}) do
 
+    user_id = Integer.to_string(get_session(conn, :user_id))
+
+    access_token = get_session(conn, :access_token)
+    headers = [{:"Authorization", "Bearer #{access_token}"}, {:"Content-Type", "application/json"}]
+
+    {:ok, response} = HTTPoison.get "http://host.docker.internal:4001/api/permissions/users/" <> user_id <> "/boards/" <> board_id, headers
+    {:ok, permission_body} = response.body |> Jason.decode()
+
+    permission = permission_body["data"]
+
     conn = put_session(conn, :board_id, board_id)
     conn = put_session(conn, :board_title, board_title)
+    conn = put_session(conn, :permission, permission["permission"])
 
     redirect(conn, to: Routes.live_path(FrontendWeb.Endpoint, FrontendWeb.TaskLive, board_title))
   end
