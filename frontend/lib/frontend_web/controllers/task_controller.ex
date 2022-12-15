2569fbf01740ff7a7ae3568b0c8d2b8353696412
Kriztoper Urmeneta <kriztoper.urmeneta@lawadvisor.com>
2022-12-15 23:51:06 +0800

    extract to separate API Client module that update task

--- frontend/lib/frontend_web/controllers/task_controller.ex
+++ frontend/lib/frontend_web/controllers/task_controller.ex
@@ -24,13 +24,8 @@ defmodule FrontendWeb.TaskController do
   end
 
   def update(conn, %{"list_id" => list_id, "id" => id, "task" => task_params}) do
-    body = Jason.encode! %{"task" => task_params}
 
-    access_token = get_session(conn, :access_token)
-    headers = [{:"Authorization", "Bearer #{access_token}"}, {:"Content-Type", "application/json"}]
-
-    {:ok, response} = HTTPoison.put "http://host.docker.internal:4001/api/lists/" <> list_id <> "/tasks/" <> id,
-                                    body, headers
+    TaskApiClient.update_task(conn, %{"list_id" => list_id, "id" => id, "task" => task_params})
 
     board_title = get_session(conn, :board_title)
 
