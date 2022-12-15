a108350ab6266c4abb522ac3ce2031fc4a8ccde4
Kriztoper Urmeneta <kriztoper.urmeneta@lawadvisor.com>
2022-12-15 13:37:53 +0800

    add board association to user permission schema

--- backend/lib/backend/user/board.ex
+++ backend/lib/backend/user/board.ex
@@ -7,7 +7,6 @@ defmodule Backend.User.Board do
   schema "boards" do
     field :title, :string
     field :owner_user_id, :integer
-    belongs_to :board, Board
 
     timestamps()
   end
--- backend/lib/backend/user/user_permission.ex
+++ backend/lib/backend/user/user_permission.ex
@@ -2,12 +2,14 @@ defmodule Backend.Access.UserPermission do
   use Ecto.Schema
   import Ecto.Changeset
   alias Backend.Accounts.User
+  alias Backend.User.Board
 
   @derive {Jason.Encoder, only: [:id, :user, :permission]}
 
   schema "user_permissions" do
     belongs_to :user, User
     field :permission, Ecto.Enum, values: [manage: 0, write: 1, read: 2]
+    belongs_to :board, Board
 
     timestamps()
   end
@@ -15,8 +17,8 @@ defmodule Backend.Access.UserPermission do
   @doc false
   def changeset(user_permission, attrs) do
     user_permission
-    |> cast(attrs, [:user_id, :permission])
-    |> validate_required([:permission])
+    |> cast(attrs, [:user_id, :permission, :board_id])
+    |> validate_required([:user_id, :permission, :board_id])
     |> assoc_constraint(:user)
   end
 end
