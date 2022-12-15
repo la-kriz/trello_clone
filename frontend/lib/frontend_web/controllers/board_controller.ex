0ec099b552afea506b883498fe1f09d40a579d1b
Kriztoper Urmeneta <kriztoper.urmeneta@lawadvisor.com>
2022-12-15 13:27:36 +0800

    initial implem for create board action

--- frontend/lib/frontend_web/controllers/board_controller.ex
+++ frontend/lib/frontend_web/controllers/board_controller.ex
@@ -31,4 +31,18 @@ defmodule FrontendWeb.BoardController do
     redirect(conn, to: Routes.board_path(conn, :index))
   end
 
+  def create(conn, %{"board_title" => board_title}) do
+
+    user_id = get_session(conn, :user_id)
+
+    body = Jason.encode! %{"title" => board_title, "owner_user_id" => user_id}
+
+    access_token = get_session(conn, :access_token)
+    headers = [{:"Authorization", "Bearer #{access_token}"}, {:"Content-Type", "application/json"}]
+
+    {:ok, _response} = HTTPoison.post "http://host.docker.internal:4001/api/boards/create", body, headers
+
+    redirect(conn, to: Routes.live_path(FrontendWeb.Endpoint, FrontendWeb.TaskLive, board_title))
+  end
+
 end
--- frontend/lib/frontend_web/router.ex
+++ frontend/lib/frontend_web/router.ex
@@ -28,6 +28,7 @@ defmodule FrontendWeb.Router do
 
     get "/", BoardController, :index
     post "/boards", BoardController, :show
+    post "/boards/create", BoardController, :create
 
     delete "/lists/:list_id/", ListController, :delete
     get "/lists/new", ListController, :new
