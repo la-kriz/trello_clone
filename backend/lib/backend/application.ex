1e91788f54b8b16b8ed759a711dfd178e28b2a14
Kriztoper Urmeneta <kriztoper.urmeneta@lawadvisor.com>
2022-12-16 11:13:10 +0800

    remove unused function in application.ex

--- backend/lib/backend/application.ex
+++ backend/lib/backend/application.ex
@@ -25,26 +25,6 @@ defmodule Backend.Application do
     Supervisor.start_link(children, opts)
   end
 
-  def start(:normal, []) do
-    children = [
-      # Start the Ecto repository
-      Backend.Repo,
-      # Start the Telemetry supervisor
-      BackendWeb.Telemetry,
-      # Start the PubSub system
-      {Phoenix.PubSub, name: Backend.PubSub},
-      # Start the Endpoint (http/https)
-      BackendWeb.Endpoint
-      # Start a worker by calling: Backend.Worker.start_link(arg)
-      # {Backend.Worker, arg}
-    ]
-
-    # See https://hexdocs.pm/elixir/Supervisor.html
-    # for other strategies and supported options
-    opts = [strategy: :one_for_one, name: Backend.Supervisor]
-    Supervisor.start_link(children, opts)
-  end
-
   # Tell Phoenix to update the endpoint configuration
   # whenever the application is updated.
   def config_change(changed, _new, removed) do
